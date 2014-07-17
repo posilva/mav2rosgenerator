ce9f1393d8ee4a2431cc5bcee0e8ad58947f7a04
Pedro Marques da Silva <posilva@academiafa.edu.pt>
2014-07-17 22:46:16 +0100

    Change setup.py to create a new package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='mav2rosgenerator',
-    version='0.1.1',
+    version='0.1.2',
     author='Pedro Marques da Silva',
     author_email='posilva@gmail.com',
     packages=['mav2ros', 'mav2ros.test'],
@@ -15,6 +15,6 @@ setup(
     url='http://pypi.python.org/pypi/mav2rosgenerator/',
     license='LICENSE.txt',
     description='A Mavlink protocol to ROS API Generator.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[],
 )
