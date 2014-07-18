a9346041c87578da45e32de3583471175403c359
Pedro Marques da Silva <posilva@academiafa.edu.pt>
2014-07-18 21:12:00 +0100

    Fix README.md include in the python package hosted at pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='mav2rosgenerator',
-    version='0.1.2',
+    version='0.1.4',
     author='Pedro Marques da Silva',
     author_email='posilva@gmail.com',
     packages=['mav2ros', 'mav2ros.test'],
